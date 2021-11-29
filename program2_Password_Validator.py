e37c0432813924f9bef66b81045c7f642a4d631e
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-29 16:50:12 +0800

    Counting of letters is working. I print the value at the end to easily check if it works.

---
+++ program2_Password_Validator.py
@@ -0,0 +1,29 @@
+# Program 2: Password validator
+# Create a program that check if password is valid
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special char (!@#$%^&*()_+ etc)
+# ex.
+# input: P@ssw0rd+P@ssw0rd
+# ouput: Valid
+
+def get_input():
+    password_input = input("\nEnter password: ")
+    return password_input
+
+
+def count_letters(letter_numbers):
+    number_count = 0
+    for n in letter_numbers:
+        if n.isalpha():
+            number_count = number_count + 1
+
+    if number_count >= 16:
+        return True 
+
+
+password = get_input()
+valid_letter_number = count_letters(password)
+print(valid_letter_number)
